9f218899478f29062c386176a2680f66d2a965f7
Morphyish <thomas.sauques@gmail.com>
2020-01-13 14:43:15 +0100

    :sparkles: Added element summary/details

---
+++ src/List/ItemDetails.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let element = {}
+
+    let keys = []
+
+    $: keys = Object.keys(element).filter(key => element.hasOwnProperty(key) && element[key] !== undefined)
+</script>
+
+<ul>
+    {#each keys as key (key)}
+        <li>{key}: <strong>{element[key]}</strong></li>
+    {/each}
+</ul>
