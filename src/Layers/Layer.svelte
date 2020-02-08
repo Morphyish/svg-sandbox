cf87e9e1c1e544d73e93c93dde2a903fb15c137e
Morphyish <thomas.sauques@gmail.com>
2020-02-08 11:57:27 +0100

    :sparkles: Added an indicator for the selected element

--- src/Layers/Layer.svelte
+++ src/Layers/Layer.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <li>
-    <div class="menuitem" role="button" tabindex="0" on:click={handleClick}>
+    <div class="menuitem" class:selected={$detail === element} role="button" tabindex="0" on:click={handleClick}>
         {element.tagName}
     </div>
 </li>
@@ -16,11 +16,16 @@
     .menuitem {
         padding: .25rem;
         cursor: pointer;
+        border-left: 3px solid transparent;
     }
 
     .menuitem:hover,
     .menuitem:focus,
     .menuitem:active {
         color: var(--primary);
+    }
+
+    .menuitem.selected {
+        border-left: 3px solid var(--primary);
     }
 </style>
